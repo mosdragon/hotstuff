7eae5b98a77d27406e74c77b3ee66a84092dae1f
Osama Sakhi <msakhi21@gmail.com>
2015-08-29 09:48:37 -0400

    Init repository

---
+++ app/src/androidTest/java/es/sakhi/osama/hotstuff/ApplicationTest.java
@@ -0,0 +1,13 @@
+package es.sakhi.osama.hotstuff;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
