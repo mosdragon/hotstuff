e4f423f820e73196e68ed7aa61d947dd75df0975
Joshua Sterling <jsterling7773@gmail.com>
2015-08-29 14:58:20 -0400

    saving data

--- app/src/main/java/es/sakhi/osama/hotstuff/ConfigurationActivity.java
+++ app/src/main/java/es/sakhi/osama/hotstuff/ConfigurationActivity.java
@@ -1,9 +1,16 @@
 package es.sakhi.osama.hotstuff;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
 
 
 public class ConfigurationActivity extends ActionBarActivity {
@@ -34,5 +41,53 @@ public class ConfigurationActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void saveConfigs(View v) {
+        //Defrost
+        //Front Window
+        Switch frontWindow = (Switch) findViewById(R.id.Front_Window);
+        boolean shouldFrontWindow = frontWindow.isChecked();
+        //Back Window
+        Switch backWindow = (Switch) findViewById(R.id.Rear_Window);
+        boolean shouldBackWindow = backWindow.isChecked();
+        //Auto
+        Switch autoDefrost = (Switch) findViewById(R.id.Auto_Defrost);
+        boolean shouldAutoDefrost = autoDefrost.isChecked();
+
+        // Car Temperature
+        // Set Car Temperature
+        SeekBar temp = (SeekBar) findViewById(R.id.Set_Temp);
+        int tempValue = temp.getVerticalScrollbarPosition();
+        //Auto Temp
+        Switch autoTemp = (Switch) findViewById(R.id.Auto_Temp);
+        boolean shouldAutoTemp = autoTemp.isChecked();
+
+        //Fuel
+        boolean findCloseGas = false;
+        boolean findCheapGas = false;
+
+        RadioGroup gasOptions = (RadioGroup) findViewById(R.id.Fuel_Group);
+        if (gasOptions.isActivated()) {
+            int optionChecked = gasOptions.getCheckedRadioButtonId();
+            RadioButton buttonChecked = (RadioButton) findViewById(optionChecked);
+            String buttonName = (String) buttonChecked.getText();
+
+            if (buttonName.equals("Cheapest Within a 10 Mile Range")) {
+                findCheapGas = true;
+            } else if (buttonName.equals("Closest To The Route")) {
+                findCloseGas = true;
+            }
+        }
+        //Additional Features
+        //Heated Seats
+        Switch heatSeats = (Switch) findViewById(R.id.Heated_Seats);
+        boolean shouldHeatSeats = heatSeats.isActivated();
+        //Cooled Seats
+        Switch coolSeats = (Switch) findViewById(R.id.Cooled_Seats);
+        boolean shouldCoolSeats = coolSeats.isActivated();
+
+        finish();
+
     }
 }
